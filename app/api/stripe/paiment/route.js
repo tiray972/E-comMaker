824202344d806252cf543c4a15d4a0322a97d0bd
RAYAN <tiray972@gmail.com>
2025-05-22 14:02:53 +0200

    rm route

--- app/api/stripe/paiment/route.js
+++
@@ -1,8 +0,0 @@
-const paymentIntent = await stripe.paymentIntents.create({
-  amount: "",
-  currency: 'eur',
-  application_fee_amount: "..." , // ta commission
-  transfer_data: {
-    destination: 'acct_xxx', // ID Stripe du vendeur
-  },
-});
