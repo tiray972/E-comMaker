e826f2fc488a7ae523fdbfe2469a73c9ae1d02cb
RAYAN <tiray972@gmail.com>
2025-05-22 15:47:59 +0200

    update stripe

--- app/stripe/test/page.tsx
+++ app/stripe/test/page.tsx
@@ -14,7 +14,7 @@ export default function StripeTestPage() {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-          connectedAccountId: 'acct_1RRVdMPu9vr1dayc',
+          connectedAccountId: 'acct_1RRYTyQ9pai1D0sw',
           line_items: [
             {
               price_data: {
